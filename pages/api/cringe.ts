f498b5941abb98d75d4b85f9d2f3dafe15811c08
Florian Walther <30524875+FlorianWalther@users.noreply.github.com>
2023-01-19 13:15:43 +0100

    Project finished

---
+++ pages/api/cringe.ts
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const prompt = req.query.prompt;
+
+  if (!prompt) {
+    return res.status(400).json({ error: "Prompt missing" });
+  }
+
+  if (prompt.length > 100) {
+    return res.status(400).json({ error: "Prompt too long" });
+  }
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Create a cringy motivational quote based on the following topic.\n
+    Topic: ${prompt}\n
+    Cringy motivational quote:`,
+    max_tokens: 500,
+    temperature: 1,
+    presence_penalty: 0,
+    frequency_penalty: 0,
+  });
+
+  const quote = completion.data.choices[0].text;
+
+  res.status(200).json({ quote });
+}
